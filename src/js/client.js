cd7085f0b5dba977061e9ea552493943f2833fcc
samanthaduarte <samanthamdc97@gmail.com>
2018-07-15 17:20:08 -0600

    ya hay x y o

--- src/js/client.js
+++ src/js/client.js
@@ -10,7 +10,7 @@ const render = lState => {
   const table = document.createElement('table');
   table.border = 0;
 
-  let turn = 'x';
+  let turn = "X";
 
   const message = document.createElement('div');
   message.className = 'message';
@@ -46,25 +46,27 @@ const render = lState => {
   const td9 = document.createElement('td');
   td9.className = 'square';
 
-  
-  const x1 = false;
-  const x2 = false;
-  const x3 = false;
-  const x4 = false;
-  const x5 = false;
-  const x6 = false;
-  const x7 = false;
-  const x8 = false;
-  const x9 = false;
-  const o1 = false;
-  const o2 = false;
-  const o3 = false;
-  const o4 = false;
-  const o5 = false;
-  const o6 = false;
-  const o7 = false;
-  const o8 = false;
-  const o9 = false;
+  let counterX = 0;
+  let counterO = 0;
+
+  let x1 = false;
+  let x2 = false;
+  let x3 = false;
+  let x4 = false;
+  let x5 = false;
+  let x6 = false;
+  let x7 = false;
+  let x8 = false;
+  let x9 = false;
+  let o1 = false;
+  let o2 = false;
+  let o3 = false;
+  let o4 = false;
+  let o5 = false;
+  let o6 = false;
+  let o7 = false;
+  let o8 = false;
+  let o9 = false;
 
   // Clear previous root content
   if (root.hasChildNodes()) {
@@ -93,76 +95,168 @@ const render = lState => {
   root.appendChild(button);
 
 
-  /*
-  const lightElements = lState.lights.map(
-    (lightColor, i) => {
-      const lightElement = document.createElement('div');
-      lightElement.className = `light ${lightColor}`;
-
-      if (i === lState.currentLight) {
-        lightElement.classList.add('on');
-      }
-
-      return lightElement;
-    }
-  );
-
-  lightElements.forEach(
-    lightElement => semaforo.appendChild(lightElement)
-  );
-*/
   // Events
   button.onclick = () => {
     render(lState);
     console.log('reinicio');
 
+
   };
 
   td1.onclick = () => {
-    console.log('casilla');
-    td1.classList.add('onX');
- 
+    console.log(turn);
+    if(turn == "X"){
+        if(x1 == false || o1 == false || o1 == false){
+          td1.classList.add('onX');    
+          turn = "O";
+          x1 = true;
+        }
+      }else{
+        if(o1 == false){
+          td1.classList.add('onO');
+          turn = "X";
+          o1 = true;
+        }
+      }  
   };
+  
   td2.onclick = () => {
-    console.log('casilla');
-    td2.classList.add('onX');
- 
+    console.log(turn);
+    if(turn == "X"){
+        if(x2 == false || o2 == false){
+          td2.classList.add('onX');    
+          turn = "O";
+          x2 = true;
+        }
+      }else{
+        if(o2 == false){
+          td2.classList.add('onO');
+          turn = "X";
+          o2 = true;
+        }
+      }  
   };
   td3.onclick = () => {
-    console.log('casilla');
-    td3.classList.add('onX');
- 
+    console.log(turn);
+    if(turn == "X"){
+        if(x3 == false || o3 == false){
+          td3.classList.add('onX');    
+          turn = "O";
+          x3 = true;
+        }
+      }else{
+        if(o3 == false){
+          td3.classList.add('onO');
+          turn = "X";
+          o3 = true;
+        }
+      }  
   };
   td4.onclick = () => {
-    console.log('casilla');
-    td4.classList.add('onX');
- 
+    console.log(turn);
+    if(turn == "X"){
+        if(x4 == false || o4 == false){
+          td4.classList.add('onX');    
+          turn = "O";
+          x4 = true;
+        }
+      }else{
+        if(o4 == false){
+          td4.classList.add('onO');
+          turn = "X";
+          o4 = true;
+        }
+      }  
   };
   td5.onclick = () => {
-    console.log('casilla');
-    td5.classList.add('onX');
- 
+    console.log(turn);
+    if(turn == "X"){
+        if(x5 == false || o5 == false){
+          td5.classList.add('onX');    
+          turn = "O";
+          x5 = true;
+        }
+      }else{
+        if(o5 == false){
+          td5.classList.add('onO');
+          turn = "X";
+          o5 = true;
+        }
+      }  
   };
   td6.onclick = () => {
-    console.log('casilla');
-    td6.classList.add('onX');
- 
+    console.log(turn);
+    if(turn == "X"){
+        if(x6 == false || o6 == false){
+          td6.classList.add('onX');    
+          turn = "O";
+          x6 = true;
+        }
+      }else{
+        if(o6 == false){
+          td6.classList.add('onO');
+          turn = "X";
+          o6 = true;
+        }
+      }  
   };
   td7.onclick = () => {
-    console.log('casilla');
-    td7.classList.add('onX');
- 
+    console.log(turn);
+    if(turn == "X"){
+        if(x7 == false || o7 == false){
+          td7.classList.add('onX');    
+          turn = "O";
+          x7 = true;
+        }
+      }else{
+        if(o7 == false){
+          td7.classList.add('onO');
+          turn = "X";
+          o7 = true;
+        }
+      }  
   };
   td8.onclick = () => {
-    console.log('casilla');
-    td8.classList.add('onX');
- 
+    console.log(turn);
+    if(turn == "X"){
+        if(x8 == false || o8 == false){
+          td8.classList.add('onX');    
+          turn = "O";
+          x8 = true;
+        }
+      }else{
+        if(o8 == false){
+          td8.classList.add('onO');
+          turn = "X";
+          o8 = true;
+        }
+      }  
   };
   td9.onclick = () => {
-    console.log('casilla');
-    td9.classList.add('onX');
- 
-  };
+    console.log(turn);
+    if(turn == "X"){
+        if(x9 == false || o9 == false){
+          td9.classList.add('onX');    
+          turn = "O";
+          x9 = true;
+        }
+      }else{
+        if(o9 == false){
+          td9.classList.add('onO');
+          turn = "X";
+          o9 = true;
+        }
+      }  
+    };
+
+  // Won or lost
+  if (o1 == true){
+    message.innerHTML = "O HA GANADO";
+    console.log("O ha ganado");
+  }
+  
 }
 
 render(state);
+
+
