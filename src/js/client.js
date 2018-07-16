de7eda154dcb72c0256c345b789fe64270c6aeef
samanthaduarte <samanthamdc97@gmail.com>
2018-07-15 23:21:58 -0600

    ganadores

--- src/js/client.js
+++ src/js/client.js
@@ -67,7 +67,8 @@ const render = lState => {
   let o7 = false;
   let o8 = false;
   let o9 = false;
-
+  
+  
   // Clear previous root content
   if (root.hasChildNodes()) {
     root.innerHTML = null;
@@ -94,167 +95,288 @@ const render = lState => {
 
   root.appendChild(button);
 
+  // Won or lost
+  let won = () => {
+    
+    // O
+    if (o1 == true && o2 == true && o3 == true){
+      message.innerHTML = "O HA GANADO";
+      console.log("O ha ganado");
+      td1.classList.add('won');
+      td2.classList.add('won');
+      td3.classList.add('won');
+    }
+    if (o4 == true && o5 == true && o6 == true){
+      message.innerHTML = "O HA GANADO";
+      console.log("O ha ganado");
+      td4.classList.add('won');
+      td5.classList.add('won');
+      td6.classList.add('won');
+    }
+    if (o7 == true && o8 == true && o9 == true){
+      message.innerHTML = "O HA GANADO";
+      console.log("O ha ganado");
+      td7.classList.add('won');
+      td8.classList.add('won');
+      td9.classList.add('won');
+    }
+
+    if (o1 == true && o4 == true && o7 == true){
+      message.innerHTML = "O HA GANADO";
+      console.log("O ha ganado");
+      td1.classList.add('won');
+      td4.classList.add('won');
+      td7.classList.add('won');
+    }
+    if (o2 == true && o5 == true && o8 == true){
+      message.innerHTML = "O HA GANADO";
+      console.log("O ha ganado");
+      td2.classList.add('won');
+      td5.classList.add('won');
+      td8.classList.add('won');
+    }
+    if (o3 == true && o6 == true && o9 == true){
+      message.innerHTML = "O HA GANADO";
+      console.log("O ha ganado");
+      td3.classList.add('won');
+      td6.classList.add('won');
+      td9.classList.add('won');
+    }
+
+    if (o1 == true && o5 == true && o9 == true){
+      message.innerHTML = "O HA GANADO";
+      console.log("O ha ganado");
+      td1.classList.add('won');
+      td5.classList.add('won');
+      td9.classList.add('won');
+    }
+    if (o7 == true && o5 == true && o3 == true){
+      message.innerHTML = "O HA GANADO";
+      console.log("O ha ganado");
+      td7.classList.add('won');
+      td5.classList.add('won');
+      td3.classList.add('won');
+    }
+
+
+    // X
+    if (x1 == true && x2 == true && x3 == true){
+      message.innerHTML = "X HA GANADO";
+      console.log("X ha ganado");
+      td1.classList.add('won');
+      td2.classList.add('won');
+      td3.classList.add('won');
+    }
+    if (x4 == true && x5 == true && x6 == true){
+      message.innerHTML = "X HA GANADO";
+      console.log("X ha ganado");
+      td4.classList.add('won');
+      td5.classList.add('won');
+      td6.classList.add('won');
+    }
+    if (x7 == true && x8 == true && x9 == true){
+      message.innerHTML = "X HA GANADO";
+      console.log("X ha ganado");
+      td7.classList.add('won');
+      td8.classList.add('won');
+      td9.classList.add('won');
+    }
+
+    if (x1 == true && x4 == true && x7 == true){
+      message.innerHTML = "X HA GANADO";
+      console.log("X ha ganado");
+      td1.classList.add('won');
+      td4.classList.add('won');
+      td7.classList.add('won');
+    }
+    if (x2 == true && x5 == true && x8 == true){
+      message.innerHTML = "X HA GANADO";
+      console.log("X ha ganado");
+      td2.classList.add('won');
+      td5.classList.add('won');
+      td8.classList.add('won');
+    }
+    if (x3 == true && x6 == true && x9 == true){
+      message.innerHTML = "X HA GANADO";
+      console.log("X ha ganado");
+      td3.classList.add('won');
+      td6.classList.add('won');
+      td9.classList.add('won');
+    }
+
+    if (x1 == true && x5 == true && x9 == true){
+      message.innerHTML = "X HA GANADO";
+      console.log("X ha ganado");
+      td1.classList.add('won');
+      td5.classList.add('won');
+      td9.classList.add('won');
+    }
+    if (x7 == true && x5 == true && x3 == true){
+      message.innerHTML = "X HA GANADO";
+      console.log("X ha ganado");
+      td7.classList.add('won');
+      td5.classList.add('won');
+      td3.classList.add('won');
+    }
+
+  }
 
   // Events
   button.onclick = () => {
     render(lState);
     console.log('reinicio');
 
-
   };
 
   td1.onclick = () => {
     console.log(turn);
-    if(turn == "X"){
-        if(x1 == false || o1 == false || o1 == false){
-          td1.classList.add('onX');    
-          turn = "O";
-          x1 = true;
-        }
-      }else{
-        if(o1 == false){
-          td1.classList.add('onO');
-          turn = "X";
-          o1 = true;
-        }
-      }  
-  };
+      if(turn == "X"){
+        td1.classList.add('onX');    
+        turn = "O";
+        x1 = true;
+      }
+      else{
+        td1.classList.add('onO');    
+        turn = "X";
+        o1 = true;
+      }
+      won();
+      console.log(x1,o1);
+  }; 
   
   td2.onclick = () => {
     console.log(turn);
-    if(turn == "X"){
-        if(x2 == false || o2 == false){
-          td2.classList.add('onX');    
-          turn = "O";
-          x2 = true;
-        }
-      }else{
-        if(o2 == false){
-          td2.classList.add('onO');
-          turn = "X";
-          o2 = true;
-        }
-      }  
+      if(turn == "X"){
+        td2.classList.add('onX');    
+        turn = "O";
+        x2 = true;
+      }
+      else{
+        td2.classList.add('onO');    
+        turn = "X";
+        o2 = true;
+      }
+      won();
+      console.log(x2,o2);
   };
+  
   td3.onclick = () => {
     console.log(turn);
-    if(turn == "X"){
-        if(x3 == false || o3 == false){
-          td3.classList.add('onX');    
-          turn = "O";
-          x3 = true;
-        }
-      }else{
-        if(o3 == false){
-          td3.classList.add('onO');
-          turn = "X";
-          o3 = true;
-        }
-      }  
+      if(turn == "X"){
+        td3.classList.add('onX');    
+        turn = "O";
+        x3 = true;
+      }
+      else{
+        td3.classList.add('onO');    
+        turn = "X";
+        o3 = true;
+      }
+      won();
+      console.log(x3,o3);
   };
+  
   td4.onclick = () => {
     console.log(turn);
-    if(turn == "X"){
-        if(x4 == false || o4 == false){
-          td4.classList.add('onX');    
-          turn = "O";
-          x4 = true;
-        }
-      }else{
-        if(o4 == false){
-          td4.classList.add('onO');
-          turn = "X";
-          o4 = true;
-        }
-      }  
+      if(turn == "X"){
+        td4.classList.add('onX');    
+        turn = "O";
+        x4 = true;
+      }
+      else{
+        td4.classList.add('onO');    
+        turn = "X";
+        o4 = true;
+      }
+      won();
+      console.log(x4,o4);
   };
+  
   td5.onclick = () => {
     console.log(turn);
-    if(turn == "X"){
-        if(x5 == false || o5 == false){
-          td5.classList.add('onX');    
-          turn = "O";
-          x5 = true;
-        }
-      }else{
-        if(o5 == false){
-          td5.classList.add('onO');
-          turn = "X";
-          o5 = true;
-        }
-      }  
+      if(turn == "X"){
+        td5.classList.add('onX');    
+        turn = "O";
+        x5 = true;
+      }
+      else{
+        td5.classList.add('onO');    
+        turn = "X";
+        o5 = true;
+      }
+      won();
+      console.log(x5,o5);
   };
+  
   td6.onclick = () => {
     console.log(turn);
-    if(turn == "X"){
-        if(x6 == false || o6 == false){
-          td6.classList.add('onX');    
-          turn = "O";
-          x6 = true;
-        }
-      }else{
-        if(o6 == false){
-          td6.classList.add('onO');
-          turn = "X";
-          o6 = true;
-        }
-      }  
+      if(turn == "X"){
+        td6.classList.add('onX');    
+        turn = "O";
+        x6 = true;
+      }
+      else{
+        td6.classList.add('onO');    
+        turn = "X";
+        o6 = true;
+      }
+      won();
+      console.log(x6,o6);
   };
+  
   td7.onclick = () => {
     console.log(turn);
-    if(turn == "X"){
-        if(x7 == false || o7 == false){
-          td7.classList.add('onX');    
-          turn = "O";
-          x7 = true;
-        }
-      }else{
-        if(o7 == false){
-          td7.classList.add('onO');
-          turn = "X";
-          o7 = true;
-        }
-      }  
+      if(turn == "X"){
+        td7.classList.add('onX');    
+        turn = "O";
+        x7 = true;
+      }
+      else{
+        td7.classList.add('onO');    
+        turn = "X";
+        o7 = true;
+      }
+      won();
+      console.log(x7,o7);
   };
+  
   td8.onclick = () => {
     console.log(turn);
-    if(turn == "X"){
-        if(x8 == false || o8 == false){
-          td8.classList.add('onX');    
-          turn = "O";
-          x8 = true;
-        }
-      }else{
-        if(o8 == false){
-          td8.classList.add('onO');
-          turn = "X";
-          o8 = true;
-        }
-      }  
+      if(turn == "X"){
+        td8.classList.add('onX');    
+        turn = "O";
+        x8 = true;
+      }
+      else{
+        td8.classList.add('onO');    
+        turn = "X";
+        o8 = true;
+      }
+      won();
+      console.log(x8,o8);
   };
+  
   td9.onclick = () => {
     console.log(turn);
-    if(turn == "X"){
-        if(x9 == false || o9 == false){
-          td9.classList.add('onX');    
-          turn = "O";
-          x9 = true;
-        }
-      }else{
-        if(o9 == false){
-          td9.classList.add('onO');
-          turn = "X";
-          o9 = true;
-        }
-      }  
-    };
-
-  // Won or lost
-  if (o1 == true){
-    message.innerHTML = "O HA GANADO";
-    console.log("O ha ganado");
-  }
+      if(turn == "X"){
+        td9.classList.add('onX');    
+        turn = "O";
+        x9 = true;
+      }
+      else{
+        td9.classList.add('onO');    
+        turn = "X";
+        o9 = true;
+      }
+      won();
+      console.log(x9,o9);
+  };
   
+
+  
+
+  
+   
 }
 
 render(state);
