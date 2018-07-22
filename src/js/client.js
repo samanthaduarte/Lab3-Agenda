2b33b41ffaf0ab52e4654121e813251e23102eda
samanthaduarte <samanthamdc97@gmail.com>
2018-07-22 01:24:49 -0600

    selected

--- src/js/client.js
+++ src/js/client.js
@@ -1,5 +1,6 @@
 const state = {
   taskList: [],
+  currentTask: 0,
   filters: ['all', 'completed','active'],
   currentFilter: 'all',
   loading: true,
@@ -22,7 +23,7 @@ const render = lState => {
 
   const topBar = document.createElement('div');
   topBar.className = 'topbar';
-  const tasks = document.createElement('ul');
+  let tasks = document.createElement('ul');
   tasks.className = 'tasks';
   const bottomBar = document.createElement('div');
   bottomBar.className = 'bottombar';
@@ -37,43 +38,43 @@ const render = lState => {
   activeBtn.className = 'button';
   activeBtn.innerHTML = 'Active';
 
+  const addBtn = document.createElement('button');
+  addBtn.className = 'addbutton';
+  addBtn.innerHTML = 'Add';
+
   let taskInput = document.createElement('input');
   taskInput.className = 'taskinput';
   taskInput.setAttribute("type", "text");
   let taskTxt;
-  const addBtn = document.createElement('button');
-  addBtn.className = 'addbutton';
-  addBtn.innerHTML = 'Add';
 
   // Clear previous root content
   if (root.hasChildNodes()) {
     root.innerHTML = null;
   }
+  
   // Main rendering
   root.appendChild(topBar);
   root.appendChild(tasks);
   root.appendChild(bottomBar);
-
   topBar.appendChild(allBtn);
   topBar.appendChild(completedBtn);
   topBar.appendChild(activeBtn);
-
-  //tasks.appendChild(task);
-
   bottomBar.appendChild(taskInput);
   bottomBar.appendChild(addBtn);
 
   // Events
   
   addBtn.onclick = () => {
+
     taskTxt = taskInput.value;
     console.log(`Se agrego una tarea ${taskTxt}`);
 
-    // Add task to array and add nonselected attribute
-    let isSelected = false;
-    let elementTask = new Array(taskTxt, isSelected);
+    // Delete input box content
+    taskInput.value = '';
+
+    // Add task to array and add nonselected value
+    let elementTask = new Array(taskTxt, false);
     lState.taskList.push(elementTask);
-    lState.currentFilter
     console.log(lState.taskList);
 
     // Create element to show task
@@ -86,27 +87,61 @@ const render = lState => {
       // Show task in different color
       task.classList.add('selected');
       // Change isSelected value to true
-      isSelected = true;
+      elementTask[1] = true;
       console.log(lState.taskList);
-    };
-  };
 
+    };
+
+    // Show all tasks
+    allBtn.onclick = () => {
+      console.log("all");
+      lState.currentFilter = lState.filters['all'];
+      render(lState);
+      for(let i = 0; i<lState.taskList.length; i++){
+        console.log(lState.taskList[i][0]);
+        // Create element to show task from array
+        task.textContent  = lState.taskList[i][0];
+        console.log(task.textContent);
+        tasks.appendChild(task);
+      }
+    };
+
+  };
+/*
   // Show all tasks
   allBtn.onclick = () => {
     console.log("all");
     lState.currentFilter = lState.filters['all'];
+    render(lState);
+    for(let i = 0; i<lState.taskList.length; i++){
+      console.log(lState.taskList[i]);
+      // Create element to show task
+      let task = document.createElement('li');
+      task.textContent  = lState.taskList[i][0];
+      tasks.appendChild(task);
+    }
   };
 
+*/
   // Show all selected tasks
   completedBtn.onclick = () => {
     console.log("completed");
     lState.currentFilter = lState.filters['completed'];
+    render(lState);
+    tasks.appendChild(document.getElementsByClassName('selected'));
   };
 
   // Show all non selected tasks
   activeBtn.onclick = () => {
     console.log("active");
     lState.currentFilter = lState.filters['active'];
+    render(lState);
+    for(let i = 0; i<lState.taskList.length; i++){
+      if (lState.taskList[i]){
+        tasks.appendChild();
+      }
+    }
+    
   };
 
 
